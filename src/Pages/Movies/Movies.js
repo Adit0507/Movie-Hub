e2de8bb0e7ea9eb977794750dd6838ab1ae7c14d
Adit0507 <adi05072002@gmail.com>
2022-07-05 21:07:28 +0530

    Cleared

--- src/Pages/Movies/Movies.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Movies = () => {
-  return (
-    <div className='pageTitle'>Movies</div>
-  )
-}
-
-export default Movies
--- src/Pages/Series/Series.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Series = () => {
-  return (
-    <div className="pageTitle">Series</div>
-  )
-}
-
-export default Series
