5f0447a4d0133e840d8a30474ef9ae18a19cb51b
Adit0507 <adi05072002@gmail.com>
2022-07-07 10:44:10 +0530

    Half done with Content Modal

--- src/components/ContentModal/ContentModal.js
+++ src/components/ContentModal/ContentModal.js
@@ -34,12 +34,115 @@ export default function TransitionModal({ children, media_type, id }) {
   const [content, setContent] = useState();
   const [video, setVideo] = useState();
 
-  const handleOpen = () =>{
+  const handleOpen = () => {
     setOpen(true);
   };
 
-  const handleClose = () =>{
-    
-  }
+  const handleClose = () => {
+    setOpen(false);
+  };
 
+  const fetchData = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/${media_type}/${id}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+    );
+
+    setContent(data);
+  };
+
+  const fetchVideo = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/${media_type}/${id}/videos?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+    );
+
+    setVideo(data.results[0]?.key);
+  };
+
+  useEffect(() => {
+    fetchData();
+    fetchVideo();
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <>
+      <div
+        className="media"
+        style={{ cursor: "pointer" }}
+        color="inherit"
+        onClick={handleOpen}
+      >
+        {children}
+      </div>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{ timeout: 500 }}
+      >
+        <Fade in={open}>
+          {content && (
+            <div className={classes.paper}>
+              <div className="ContentModal">
+                <img
+                  src={
+                    content.poster_path
+                      ? `${img_500}/${content.poster_path}`
+                      : unavailable
+                  }
+                  alt={content.name || content.title}
+                  className="ContentModal_portrait"
+                />
+                <img
+                  src={
+                    content.backdrop_path
+                      ? `${img_500}/${content.backdrop_path}`
+                      : unavailableLandscape
+                  }
+                  alt={content.name || content.title}
+                  className="ContentModal_Landscape"
+                />
+                <div className="ContentModal__about">
+                  <span className="ContentModal__title">
+                    {content.name || content.title} (
+                    {(
+                      content.first_air_date ||
+                      content.release_date ||
+                      "-----"
+                    ).substring(0, 4)}
+                    )
+                  </span>
+                  {content.tagline && (
+                    <i className="tagline">{content.tagline}</i>
+                  )}
+
+                  <span className="ContentModal__description">
+                    {content.overview}
+                  </span>
+
+                  <div>
+                    <Carousel id={id} media_type={media_type} />
+                  </div>
+
+                  <Button
+                    variant="contained"
+                    startIcon={<YouTube />}
+                    color="secondary"
+                    target="_blank"
+                    href={`https://www.youtube.com/watch?v=${video}`}
+                  >
+                    Watch the trailer 😉
+                  </Button>
+                </div>
+              </div>
+            </div>
+          )}
+        </Fade>
+      </Modal>
+    </>
+  );
 }
